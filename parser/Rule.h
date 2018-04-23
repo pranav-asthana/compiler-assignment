a2ef453da25b9f344a62f3398347c860f0dd5ab5
arnav dhamija <arnav.dhamija@gmail.com>
2018-04-23 10:09:44 +0530

    More work

--- parser/Rule.h
+++ parser/Rule.h
@@ -1,1 +1,15 @@
- 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Rule {
+public:
+    string nonTerm;
+    vector<string> parsedRuleRHS;
+    Rule(string _nonTerm, vector<string> _rhs)
+    {
+        nonTerm = _nonTerm;
+        parsedRuleRHS = _rhs;
+    }
+};
